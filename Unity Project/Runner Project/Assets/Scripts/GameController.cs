35d528b95f071621207957c7d5f0f28e04771196
Uriel Carrillo <carrillouriel@gmail.com>
2017-10-15 22:32:40 -0700

    El jugador puede ganar y perder

---
+++ Unity Project/Runner Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject jugador;
+	public GameObject metaNivel;
+	public GameObject CanvasWin;
+	public GameObject CanvasLose;
+
+
+	void Start () {
+		Time.timeScale = 1.0f; 
+	}
+
+	void Update () {
+		RevisarPosicion ();
+	}
+
+	//Revisa la posición del usuario para determinar si cayó por un agujero o no.
+	void RevisarPosicion()
+	{
+		if (jugador.transform.position.y < -2) {
+			//jugador cayó por un hoyo.
+			MostrarGameOver();
+		}
+
+		if (jugador.transform.position.z >= metaNivel.transform.position.z) {
+			MostrarWin ();
+		}
+	}
+
+	//Muestra el canvas de Game Over.
+	void MostrarGameOver(){
+		CanvasLose.SetActive (true);
+		DetenerTiempo ();
+	}
+
+	//Muestra el canvas de ganador
+	void MostrarWin(){
+		CanvasWin.SetActive (true);
+		DetenerTiempo ();
+	}
+
+	public void ResetearNivel(){
+		Application.LoadLevel("Scene1");
+	}
+
+	void DetenerTiempo(){
+		Time.timeScale = 0.0f; 
+	}
+}
