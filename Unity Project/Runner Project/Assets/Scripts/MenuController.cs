d782d8e2d7263c0a4f3725c7188fca239f3bba10
tmartinez <tmartinez@switchfly.com>
2017-11-07 18:06:47 -0700

    menu v2 and refactoring on menu controller code

--- Unity Project/Runner Project/Assets/Scripts/MenuController.cs
+++ Unity Project/Runner Project/Assets/Scripts/MenuController.cs
@@ -3,85 +3,83 @@ using System.Collections.Generic;
 
 public class MenuController : MonoBehaviour
 {
-  private GameMaster master;
+	private GameMaster _master;
 
-  [SerializeField]
-  private GameObject canvasMain;
-  [SerializeField]
-  private GameObject canvasPlay;
-  [SerializeField]
-  private GameObject canvasOptions;
-  [SerializeField]
-  private GameObject canvasCredits;
+	[SerializeField] private GameObject _panelMain;
+	[SerializeField] private GameObject _panelPlay;
+	[SerializeField] private GameObject _panelOptions;
+	[SerializeField] private GameObject _panelCredits;
+	[SerializeField] private GameObject _buttonBack;
 
-  [SerializeField]
-  private GameObject baseStageButton;
+	[SerializeField] private GameObject _baseStageButton;
 
-  // Use this for initialization
-  void Start()
-  {
-    master = GameMaster.getInstance();
-    createStageButtons();
-  }
+	void Start ()
+	{
+		_master = GameMaster.getInstance ();
+		createStageButtons ();
+		resetPanels ();
+	}
 
-  public void playButtonHandler()
-  {
-    canvasMain.SetActive(false);
-    canvasPlay.SetActive(true);
-  }
+	public void playButtonHandler ()
+	{
+		_panelMain.SetActive (false);
+		_panelPlay.SetActive (true);
+		_buttonBack.SetActive (true);
+	}
 
-  public void optionsButtonHandler()
-  {
-    canvasMain.SetActive(false);
-    canvasOptions.SetActive(true);
-  }
+	public void optionsButtonHandler ()
+	{
+		_panelMain.SetActive (false);
+		_panelOptions.SetActive (true);
+		_buttonBack.SetActive (true);
+	}
 
-  public void creditsButtonHandler()
-  {
-    canvasMain.SetActive(false);
-    canvasCredits.SetActive(true);
-  }
+	public void creditsButtonHandler ()
+	{
+		_panelMain.SetActive (false);
+		_panelCredits.SetActive (true);
+		_buttonBack.SetActive (true);
+	}
 
-  public void selectStageButtonHandler(int stageIndex)
-  {
-    master.setStageIndex(stageIndex);
-    master.goToScene(2);
-  }
+	public void selectStageButtonHandler (int stageIndex)
+	{
+		_master.setStageIndex (stageIndex);
+		_master.goToScene (2);
+	}
 
-  public void backButtonHandler()
-  {
-    canvasMain.SetActive(true);
-    canvasPlay.SetActive(false);
-    canvasOptions.SetActive(false);
-    canvasCredits.SetActive(false);
-  }
+	public void exitButtonHandler ()
+	{
+		Debug.Log ("Quit");
+		Application.Quit ();
+	}
 
-  public void exitButtonHandler()
-  {
-    Debug.Log("Quit");
-    Application.Quit();
-  }
+	public void resetPanels ()
+	{
+		_panelMain.SetActive (true);
+		_panelPlay.SetActive (false);
+		_panelOptions.SetActive (false);
+		_panelCredits.SetActive (false);
+		_buttonBack.SetActive (false);
+	}
 
-  private void createStageButtons()
-  {
-    List<Stage> stages = master.getStages();
-    GameObject newStageItem;
-    Vector3 position = new Vector3(baseStageButton.transform.position.x, baseStageButton.transform.position.y, 0);
+	private void createStageButtons ()
+	{
+		List<Stage> stages = _master.getStages ();
+		GameObject newStageItem;
+		Vector3 position = new Vector3 (_baseStageButton.transform.position.x, _baseStageButton.transform.position.y, 0);
 
-    for (int i = 0; i < stages.Count; i++)
-    {
-      if (!stages[i].getIslocked())
-      {
-        newStageItem = Instantiate (baseStageButton, canvasPlay.transform, false);
-		newStageItem.transform.localPosition = new Vector3 (-260, 226, 1738); // TODO - hardcoded values, replace for relative points from prefab.
-        newStageItem.name = "s" + i;
+		for (int i = 0; i < stages.Count; i++) {
+			if (!stages [i].getIslocked ()) {
+				newStageItem = Instantiate (_baseStageButton, _panelPlay.transform, false);
+				newStageItem.transform.localPosition = new Vector3 (-260, 226, 1738); // TODO - hardcoded values, replace for relative points from prefab.
+				newStageItem.name = "s" + i;
 
-		newStageItem.GetComponent<StageMenuItem> ()
+				newStageItem.GetComponent<StageMenuItem> ()
 			.setLabel (stages [i].getLabel ())
 			.setStars (stages [i].getStars ())
 			.setLetters (stages [i].getLetters ())
 			.setStageIndex (i).setParent (this);
-      }
-    }
-  }
+			}
+		}
+	}
 }
