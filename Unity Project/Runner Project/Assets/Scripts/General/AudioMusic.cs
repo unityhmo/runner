298ca6c5a7877206aebcc7fd674656a5d472cee6
SergioMireles <sergio.mireles@evolvemediallc.com>
2017-11-25 18:33:17 -0700

    SFX, MFX and AudioManager Implementation (fix)
    - Fix on music loops bug when changing scenes. Coroutines reset were needed.

--- Unity Project/Runner Project/Assets/Scripts/General/AudioMusic.cs
+++ Unity Project/Runner Project/Assets/Scripts/General/AudioMusic.cs
@@ -5,38 +5,39 @@ public class AudioMusic : AudioObject
 {
   private AudioClip _themeIntro;
   private AudioClip _themeLoop;
-  private bool _hasTwoClips = false;
+  private IEnumerator _coroutine;
 
   public void SetClip(AudioClip loop, AudioClip intro = null)
   {
-    AudioClip clip;
-
-    _themeLoop = loop;
-    clip = _themeLoop;
-
-    if (intro != null)
+    if (_coroutine != null)
     {
-      _hasTwoClips = true;
-      _themeIntro = intro;
-      clip = _themeIntro;
+      StopCoroutine(_coroutine);
+      _coroutine = null;
     }
-
-    PlayClip(clip);
-  }
-
-  private void PlayClip(AudioClip clip)
-  {
     _source.Stop();
-    _source.clip = clip;
-    _source.Play();
 
-    if (_hasTwoClips)
-      StartCoroutine(PlayIntroLoopTrack());
+    _themeIntro = intro;
+    _themeLoop = loop;
+
+    if (_themeIntro != null)
+    {
+      _coroutine = PlayIntroLoopTrack();
+      StartCoroutine(_coroutine);
+    }
+    else
+    {
+      _source.clip = _themeLoop;
+      _source.Play();
+    }
   }
 
   IEnumerator PlayIntroLoopTrack()
   {
+    _source.clip = _themeIntro;
+    _source.Play();
+
     yield return new WaitForSeconds(_source.clip.length);
+
     _source.clip = _themeLoop;
     _source.Play();
   }
