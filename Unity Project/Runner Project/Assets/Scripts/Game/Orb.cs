b3a7e385af395f6b4892048a5d0fa4f6d4dddd00
SergioMireles <sergio.mireles@evolvemediallc.com>
2017-12-12 15:52:25 -0700

    New Pickup prefabs "tuerquitas"
    - Adding mesh and texture
    - Adding spinning effect to pickup
    - Adding special directional light that only affects pickup layer objects
    - Adding pickups to level_0

--- Unity Project/Runner Project/Assets/Scripts/Game/Orb.cs
+++ Unity Project/Runner Project/Assets/Scripts/Game/Orb.cs
@@ -2,14 +2,26 @@
 
 public class Orb : MonoBehaviour
 {
+  [SerializeField]
+  private GameObject _mesh;
+  private Renderer _rend;
+  [SerializeField]
+  private float _speed = 250f;
+
   void Awake()
   {
     GameObject gameController = GameObject.FindGameObjectWithTag(BaseValues.TAG_GAME_CONTROLLER);
 
     if (gameController)
-    {
       gameController.GetComponent<GameController>().OrbDetected();
-    }
+
+    _rend = _mesh.GetComponent<Renderer>();
+  }
+
+  void LateUpdate()
+  {
+    if (_rend.isVisible)
+      transform.Rotate(Vector3.up, _speed * Time.deltaTime);
   }
 
   public void CollisionDetected()
