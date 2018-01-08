f62dab0f338aa8af17dbf02c78ed28b4343dabbc
smireles <mireles001@gmail.com>
2018-01-07 18:42:41 -0700

    Adding particle system destroyer

---
+++ Unity Project/Runner Project/Assets/Scripts/Game/ParticleSystemDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ParticleSystemDestroyer : MonoBehaviour
+{
+  private ParticleSystem ps;
+
+  public void Awake()
+  {
+    ps = GetComponent<ParticleSystem>();
+  }
+
+  public void LateUpdate()
+  {
+    if (ps && !ps.IsAlive())
+    {
+      Destroy(gameObject);
+    }
+  }
+}
